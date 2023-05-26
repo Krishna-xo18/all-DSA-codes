c6e68dc2ec7b7065f9c5253238801802d000e185
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-05-26 12:02:23 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (81.47%) - LeetSync

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char>s1;
+        if(s[0]==')' || s[0]==']' || s[0]=='}' ){
+               return false;
+            }
+             if(s[s.size()-1]=='(' || s[s.size()-1]=='[' || s[s.size()-1]=='{' ){
+         return false;
+            }
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{' ){
+               s1.push(s[i]);
+            }
+           else  if(s1.empty()){
+                s1.push(s[i]);
+            }
+            else  if(s[i]=='}'){
+
+                 if(s1.top()=='{'){
+                 s1.pop();
+                 }
+                 else{
+                     s1.push('}');
+                 }
+
+             }
+             else  if(s[i]==')' ){
+                 
+                  if(s1.top()=='('){
+                 s1.pop();
+                 }
+                 else{
+                     s1.push(')');
+                 }
+             }
+             else  if(s[i]==']' ){
+                 if(s1.top()=='['){
+                 s1.pop();
+                 }
+                 else{
+                     s1.push(']');
+                 }
+                 
+             }
+        }
+        if(s1.empty()){
+            return true;
+        }
+        return false;
+    }
+};
