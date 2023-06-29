6a30a5f70f929187c693917d434ac628127e1811
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-06-29 12:46:46 +0530

    Time: 8 ms (6.56%) | Memory: 9.7 MB (58.14%) - LeetSync

--- invert-binary-tree/invert-binary-tree.cpp
+++ invert-binary-tree/invert-binary-tree.cpp
@@ -15,11 +15,12 @@ public:
         if(root==NULL){
             return NULL;
         }
-        invertTree(root->left);
-        invertTree(root->right);
         TreeNode*temp=root->left;
         root->left=root->right;
         root->right=temp;
+        invertTree(root->left);
+        invertTree(root->right);
+        
         return root;
     }
 };
