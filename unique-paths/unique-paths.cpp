6cd6d8601eb0db49c042f122f621fc59227d62bc
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-07-12 13:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (20.77%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>&dp){
-        //reaching start
-       if(i==0 && j==0){
-           return 1;
-       }
-       //excedding the boundaries
-       if(i<0 || j<0){
-           return 0;
-       }
-       if(dp[i][j]!=-1){
-           return dp[i][j];
-       }
-       int up=solve(i-1,j,dp);
-       int left=solve(i,j-1,dp);
-       return dp[i][j]=up+left;
+    int solve(int m,int n,vector<vector<int>>&dp){
+
+    for(int i=m-2;i>=0;i--){
+        for(int j=n-2;j>=0;j--){
+            dp[i][j]=dp[i+1][j]+dp[i][j+1];
+        }
+    }
+       return dp[0][0];
     }
     int uniquePaths(int m, int n) {
         //move down or right so if we go from bottom it will be up or left
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        vector<vector<int>>dp(m+10,vector<int>(n+10,1));
+        return solve(m,n,dp);
     }
 };
