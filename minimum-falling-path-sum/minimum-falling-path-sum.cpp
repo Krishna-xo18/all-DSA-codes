58601906cdd1e326064742f6edbb2b50c2e72d47
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-07-12 18:38:25 +0530

    Time: 10 ms (98.20%) | Memory: 10.7 MB (31.60%) - LeetSync

--- minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        // int ans=0;
-        int n= matrix.size();
-        int m= matrix[0].size();
-         vector<vector<int>> dp(n,vector<int>(m,0));
-         for(int i=0;i<m;i++){
+        int m= matrix.size();
+        int n= matrix[0].size();
+         vector<vector<int>> dp(m,vector<int>(n,0));
+         for(int i=0;i<n;i++){
            dp[0][i]=matrix[0][i];
          }
-        for(int i=0;i<m;i++){
-           cout<<dp[0][i]<<" ";
-         }
-         for(int i=1;i<n;i++){
-             for(int j=0;j<m;j++){
+         for(int i=1;i<m;i++){
+             for(int j=0;j<n;j++){
                  if(j==0){
                      dp[i][j]=matrix[i][j] + min(dp[i-1][j],dp[i-1][j+1]);
                  }
@@ -23,16 +19,12 @@ public:
                      dp[i][j]=matrix[i][j] + min(dp[i-1][j-1],min(dp[i-1][j],dp[i-1][j+1]));
                  }
              }
-              for(int k=0;k<m;k++){
-           cout<<dp[i][k]<<" ";
          }
-         }
-
          vector<int> v;
-         for(int i=0;i<m;i++){
-           v.push_back(dp[n-1][i]);
+         int ans=INT_MAX;
+         for(int i=0;i<n;i++){
+           ans=min(ans,dp[m-1][i]);
          }
-         sort(v.begin(),v.end());
-        return v[0];
+         return ans;
     }
 };
