572b7bd3134539d92e9085fe5e798a75fd2e31cf
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-07-18 12:14:07 +0530

    Time: 109 ms (66.29%) | Memory: 49.7 MB (10.35%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(int idx,int target,vector<int>&coins,vector<vector<int>>&dp){
+        if(idx==0){
+            if(target % coins[0]==0){
+                return target/coins[0];
+            }
+            return 1e9;
+        }
+        if(dp[idx][target]!=-1){
+            return dp[idx][target];
+        }
+        int nottake=0+solve(idx-1,target,coins,dp);
+        int take=1e9;
+        if(target>=coins[idx]){
+            take=1+solve(idx,target-coins[idx],coins,dp);
+        }
+        return dp[idx][target]=min(take,nottake);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        int x=solve(coins.size()-1,amount,coins,dp);
+      if(x==1e9){
+          return -1;
+      }
+      return x;
+        
+
+    }
+};
