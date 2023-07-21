1289fbaffdddf687754a832733bc2473db190702
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-07-21 17:38:20 +0530

    Time: 143 ms (9.80%) | Memory: 28.5 MB (16.84%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+    public:
+        int solve(int idx1, int idx2, string text1, string text2, vector<vector < int>> &dp)
+        {
+           	//shifting of index method shift by 1 all indices i.e -1 will become 0 ,n-1 will become n
+            idx1++;
+            idx2++;
+            for (int i = 0; i <= idx1; i++)
+            {
+                for (int j = 0; j <= idx2; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        dp[i][j] = 0;
+                    }
+                    else if (text1[i - 1] == text2[j - 1])
+                    {
+                        dp[i][j] = 1 + dp[i - 1][j - 1];
+                    }
+                    else
+                    {
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    }
+                }
+            }
+            return dp[idx1][idx2];
+        }
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        vector<vector < int>> dp(text1.size() + 1, vector<int> (text2.size() + 1, 0));
+        return solve(text1.size() - 1, text2.size() - 1, text1, text2, dp);
+    }
+    int longestPalindromeSubseq(string s)
+    {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        return longestCommonSubsequence(s2, s);
+    }
+    int minInsertions(string s)
+    {
+       	//min insertions to make palindromic =total len-len of longest palindromic subsequence;
+        return s.size() - longestPalindromeSubseq(s);
+    }
+};
