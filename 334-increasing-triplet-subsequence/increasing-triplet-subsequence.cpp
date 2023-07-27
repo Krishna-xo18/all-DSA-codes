7d17a75e9d6e89ee7ae7a091166f26c9bab83a23
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-07-27 11:51:41 +0530

    Time: 196 ms (7.33%) | Memory: 111.8 MB (33.91%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        //binary search approach
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+           if(nums[i]>temp.back())
+           {
+               temp.push_back(nums[i]);
+           }
+           else{
+               int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+               temp[idx]=nums[i];
+           }
+        }
+        return temp.size();
+    }
+    bool increasingTriplet(vector<int>& nums) {
+        if(lengthOfLIS(nums)>=3){
+            return true;
+        }
+        return false;
+    }
+};
