9f2b033cdc5fcc039a25af6bfd5f3e3c12ed5c9c
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-08-01 17:08:27 +0530

    Time: 14 ms (79.88%) | Memory: 11.9 MB (71.55%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,29 +1,36 @@
-class Solution {
-public:
-  int dp[1001][102][2];
-   int solve(int idx,int buy,vector<int>&prices,int k){
-      if(idx>=prices.size() || k<=0){
-          return 0;
-      }
-      if(dp[idx][k][buy]!=-1){
-          return dp[idx][k][buy];
-      }
-      int profit=0;
-      if(buy){
-          int take=-prices[idx]+solve(idx+1,0,prices,k);
-          int nottake=0+solve(idx+1,1,prices,k);
-          profit=max(take,nottake);
-      }
-      else{
-          int take=prices[idx]+solve(idx+1,1,prices,k-1);
-          int nottake=0+solve(idx+1,0,prices,k);
-          profit=max(take,nottake);
-      }
-     
-     return dp[idx][k][buy]=profit;
-     }
-    int maxProfit(int k, vector<int>& prices) {
-    memset(dp,-1,sizeof(dp));
-      return solve(0,1,prices,k);
+class Solution
+{
+    public:
+        int dp[1001][102][2];
+    int solve(int idx, int buy, vector<int> &prices, int k)
+    {
+        if (idx >= prices.size() || k <= 0)
+        {
+            return 0;
+        }
+        if (dp[idx][k][buy] != -1)
+        {
+            return dp[idx][k][buy];
+        }
+        int profit = 0;
+        if (buy)
+        {
+            int take = -prices[idx] + solve(idx + 1, 0, prices, k);
+            int nottake = 0 + solve(idx + 1, 1, prices, k);
+            profit = max(take, nottake);
+        }
+        else
+        {
+            int take = prices[idx] + solve(idx + 1, 1, prices, k - 1);
+            int nottake = 0 + solve(idx + 1, 0, prices, k);
+            profit = max(take, nottake);
+        }
+
+        return dp[idx][k][buy] = profit;
+    }
+    int maxProfit(int k, vector<int> &prices)
+    {
+        memset(dp, -1, sizeof(dp));
+        return solve(0, 1, prices, k);
     }
 };
