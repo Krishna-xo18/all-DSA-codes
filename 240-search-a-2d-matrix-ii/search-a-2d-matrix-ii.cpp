3401f80cf709532d3eb4ace3075ccb14b053d2c5
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-08-07 14:17:25 +0530

    Time: 626 ms (5.00%) | Memory: 157.2 MB (12.10%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        vector<int>v;
+        for(int i=0;i<matrix.size();i++){
+           v.push_back(matrix[i][0]);
+        }
+        int x=upper_bound(v.begin(),v.end(),target)-v.begin();
+        for(int j=0;j<x;j++){
+            if(binary_search(matrix[j].begin(),matrix[j].end(),target)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
