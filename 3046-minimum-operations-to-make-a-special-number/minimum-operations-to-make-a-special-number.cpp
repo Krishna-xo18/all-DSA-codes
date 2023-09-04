2638bc25d628e31c15a6962f6ee5fc7864591cd1
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-09-04 11:57:17 +0530

    Time: 3 ms (80.00%) | Memory: 6.3 MB (80.00%) - LeetSync

--- 3046-minimum-operations-to-make-a-special-number/minimum-operations-to-make-a-special-number.cpp
+++ 3046-minimum-operations-to-make-a-special-number/minimum-operations-to-make-a-special-number.cpp
@@ -13,6 +13,7 @@ public:
         }
         int digit=num[idx]-'0';
         int delete_digit=1+solve(idx+1,number,num);
+        //we do (number*10+digit)%25 because for example to check divisibility of 2573 we can check by divisibility of(2500+75 )%25 so we can check the running reminder
         int skip_digit=solve(idx+1,(number*10+digit)%25,num);
         return dp[idx][number]=min(delete_digit,skip_digit);
     }
