c60f5dd391fd6ede681fdacb9461bfd360950510
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-09-04 11:53:57 +0530

    Time: 9 ms (80.00%) | Memory: 6.3 MB (80.00%) - LeetSync

---
+++ 3046-minimum-operations-to-make-a-special-number/minimum-operations-to-make-a-special-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[101][26];
+    int solve(int idx ,int number,string &num ){
+        if(idx==num.size()){
+            if(number%25==0){
+                return 0;
+            }
+            return 1e5;
+        }
+        if(dp[idx][number]!=-1){
+            return dp[idx][number];
+        }
+        int digit=num[idx]-'0';
+        int delete_digit=1+solve(idx+1,number,num);
+        int skip_digit=solve(idx+1,(number*10+digit)%25,num);
+        return dp[idx][number]=min(delete_digit,skip_digit);
+    }
+    int minimumOperations(string num) {
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,num);
+    }
+};
