57458a6ba31330b317c95bf3c69e0ea3d032deea
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2023-09-09 11:06:08 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (86.82%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int dp[101][101][201];
+    int dp[101][101];
     int solve(int idx1,int idx2,int idx3,string &s1,string &s2,string &s3){
         if(idx1==s1.size() && idx2==s2.size() && idx3==s3.size()){
             return 1;
         }
-        if(dp[idx1][idx2][idx3]!=-1){
-            return dp[idx1][idx2][idx3];
+        if(dp[idx1][idx2]!=-1){
+            return dp[idx1][idx2];
         }
         int take1=0;
         if(idx1<s1.size() && s1[idx1]==s3[idx3]){
@@ -17,9 +17,9 @@ public:
            take2=solve(idx1,idx2+1,idx3+1,s1,s2,s3);
         }
         if(take1==0 && take2==0){
-            return dp[idx1][idx2][idx3]=0;
+            return dp[idx1][idx2]=0;
         }
-        return dp[idx1][idx2][idx3]=(take1 || take2);
+        return dp[idx1][idx2]=(take1 || take2);
     }
     bool isInterleave(string s1, string s2, string s3) {
         memset(dp,-1,sizeof(dp));
