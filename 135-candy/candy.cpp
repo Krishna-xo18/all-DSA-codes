0b4ff73d1b0e901253ce5c03392673229b5d8bf2
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2024-01-06 08:55:09 +0530

    Time: 20 ms (32.47%) | Memory: 18 MB (61.30%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -7,20 +7,25 @@ public:
         if(ratings[i]>ratings[i-1]  ){
             temp[i]=temp[i-1]+1;
         }
+        else if(ratings[i-1]>ratings[i]){
+            temp[i-1]=temp[i]+1;
+        }
         
         }
-        for(int i=ratings.size()-2;i>=0;i--){
-        if(ratings[i]>ratings[i+1]  ){
-            temp[i]=max(temp[i],temp[i+1]+1);
+        for(int i=ratings.size()-1;i>=1;i--){
+        if(ratings[i]>ratings[i-1]  ){
+            temp[i]=max(temp[i],temp[i-1]+1);
         }
-      
+        else if(ratings[i-1]>ratings[i]){
+            temp[i-1]=max(temp[i-1],temp[i]+1);
+        }
         
         }
         
         int ans=0;
         for(int i=0;i<temp.size();i++)
         {
-          //  cout<<temp[i]<<" ";
+            //cout<<temp[i]<<" ";
             ans+=temp[i];
         }
         return ans;
