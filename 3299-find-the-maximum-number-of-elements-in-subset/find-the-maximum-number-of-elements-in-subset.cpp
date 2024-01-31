2195fcc6ce04285952fbbbf2f2936080674652fe
Krishna-xo18 <117559604+Krishna-xo18@users.noreply.github.com>
2024-01-31 17:32:51 +0530

    Time: 231 ms (85.84%) | Memory: 118.4 MB (90.44%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.cpp
@@ -1,21 +1,52 @@
+typedef long long ll;
+
 class Solution {
 public:
-   int maximumLength(vector<int>& nums) {
-    long long ans = 0;
-    map<int, int> freq;
-    for(auto n: nums) freq[n]++;
-    for(auto [k,f] : freq){
-        long long t = k, count = 0;
-        if(t == 1) { count += freq[t]; freq[t] = 0; } 
-        while(t < INT_MAX && freq[t] > 0){
-            count += 2;
-            if(freq[t] == 1) break; 
-            freq[t] = 0;                 // set to zero, so that we do not check for this sequence again
-            t = t * t;
-        }
-        if(count % 2 == 0) count--;
-        ans = max(ans, count);
+    bool checkPerfectSquare(ll x) {
+        
+        if(ceil(double(sqrt(x))) == floor(double(sqrt(x)))) return true;
+        else return false;
     }
-    return ans;
-}
+    
+    int maximumLength(vector<int>& nums) {
+        
+        unordered_map<int, int> map;
+        
+        for(auto x : nums) {
+            map[x]++;
+        }
+        
+        int subLen = 1;
+        for(ll x : nums) {
+            int tempLen = 1;
+            
+            // Edge case i.e. when the current element is 1. It can 
+            // include as many odd number of 1 in its subset.
+            if(x == 1) {
+                if(map[x] != 1) subLen = max(subLen, map[x] - (map[x]%2 == 0));
+                continue;
+            }
+            
+            // First check whether the current no. itself is eligible to be a perfect square.
+            while(checkPerfectSquare(x)) {
+
+                // Then check the other condition for it predecessors
+
+                ll sqrtX = sqrt(x);
+                if(map.find(sqrtX) != map.end() && map[sqrtX] >=2) {
+                    tempLen += 2;
+                }
+                else {
+                    break;
+                }
+
+                // Reduce the peak
+                x = sqrtX;
+            }
+            
+            subLen = max(subLen, tempLen);
+        }
+        
+        return subLen;
+    }
 };
